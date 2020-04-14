9ba2048614d97a2950391cd8998c2cf63c1187ba
Michel Werren <michel.werren@husqvarnagroup.com>
2020-04-14 18:36:13 +0200

    chore: Update Javadoc of the orchestra options.

--- src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/VertxKinesisOrchestraOptions.kt
+++ src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/VertxKinesisOrchestraOptions.kt
@@ -1,6 +1,7 @@
 package ch.sourcemotion.vertx.kinesis.consumer.orchestra
 
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.OrchestrationVerticleOptions
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.vertx.core.json.JsonObject
 import io.vertx.redis.client.RedisOptions
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
@@ -10,10 +11,28 @@ import java.time.Duration
 import java.util.function.Supplier
 
 data class VertxKinesisOrchestraOptions @JvmOverloads constructor(
+    /**
+     * The name of the application this orchestra is used by. This name is represents the discriminator between different orchestras.
+     * Shard iterators are persisted on Redis per application, so it's possible that multiple application independently
+     * consumes records from the same stream.
+     */
     var applicationName: String,
+    /**
+     * Name of the stream this orchestra should consume records from.
+     */
     var streamName: String,
+    /**
+     * AWS region on which the orchestra will run.
+     */
     var region: String = DEFAULT_REGION.id(),
+    /**
+     * Interval Kinesis should be queried for new records. If the processing time of the previous received bunch of records did
+     * take longer than this interval, then Kinesis will get queried immediately when this work get done.
+     */
     var kinesisPollInterval: Duration = Duration.ofMillis(DEFAULT_KINESIS_POLL_INTERVAL),
+    /**
+     * The max. amount of records per query against Kinesis.
+     */
     var recordsPerPollLimit: Int = DEFAULT_RECORDS_PER_POLL_LIMIT,
     /**
      * Expiration of the progress flag during shard processing. If the flag is not updated within this expiration period
@@ -21,6 +40,9 @@ data class VertxKinesisOrchestraOptions @JvmOverloads constructor(
      * ungracefully shutdown of a consumer.
      */
     var shardProgressExpiration: Duration = Duration.ofMillis(DEFAULT_SHARD_PROGRESS_EXPIRATION),
+    /**
+     * Should supply the correct credential for the respectively environment.
+     */
     var credentialsProviderSupplier: Supplier<AwsCredentialsProvider> = Supplier { DefaultCredentialsProvider.create() },
     /**
      * Alternative kinesis endpoint.
@@ -32,8 +54,18 @@ data class VertxKinesisOrchestraOptions @JvmOverloads constructor(
      */
     var redisOptions: RedisOptions,
 
+    /**
+     * Strategy how and which shard iterator should be used.
+     */
     var shardIteratorStrategy: ShardIteratorStrategy = ShardIteratorStrategy.EXISTING_OR_LATEST,
+    /**
+     * Finally the definition how many max. shards an orchestra instance could / should consume.
+     */
     var loadConfiguration: LoadConfiguration = LoadConfiguration.createExactConfig(1),
+
+    /**
+     * How the orchestra should behave on failures during record processing.
+     */
     var errorHandling: ErrorHandling = ErrorHandling.RETRY_FROM_FAILED_RECORD,
 
     /**
@@ -55,13 +87,20 @@ data class VertxKinesisOrchestraOptions @JvmOverloads constructor(
      */
     var reshardingNotificationAddress: String = "/kinesis-consumer-orchester/resharding",
     /**
+     * Class of record consumer verticle. Each consumer verticle will consumer and process one shard. This means
+     * one instance per shard will get deployed and consumes records of the corresponding shard.
+     *
      * Must be the full qualified class name of an implementation of:
      * - [com.gardena.smartgarden.vertx.kinesis.consumer.orchestra.consumer.AbstractKinesisConsumerVerticle]
      * - [com.gardena.smartgarden.vertx.kinesis.consumer.orchestra.consumer.AbstractKinesisConsumerCoroutineVerticle]
      */
     var consumerVerticleClass: String,
     /**
-     * Additional configuration. They will be added to the consumer verticle ([errorHandling]) config.
+     * Additional configuration, passed as options / configuration to the deployment of [consumerVerticleClass].
+     *
+     * IMPORTANT:
+     * Be aware that this options are a combination with the internal configuration, so if you use Jackson
+     * please add @[JsonIgnoreProperties] with [JsonIgnoreProperties.ignoreUnknown] true configured.
      */
     var consumerVerticleConfig: JsonObject = JsonObject()
 ) {
@@ -93,9 +132,6 @@ data class VertxKinesisOrchestraOptions @JvmOverloads constructor(
     )
 }
 
-/**
- * Configures the handling of errors.
- */
 enum class ErrorHandling {
     /**
      * In the case of failure and the consumer throws an exception of type
@@ -112,7 +148,16 @@ enum class ErrorHandling {
 }
 
 enum class ShardIteratorStrategy {
+    /**
+     * If there is an existing shard iterator present (e.g. from a previous run), this iterator will be used (continued from).
+     * When there is no iterator information available on Redis, the latest (queried from Kinesis) will be used.
+     */
     EXISTING_OR_LATEST,
+
+    /**
+     * No check for existing iterator information. Kinesis will directly get queried for latest shard iterator, which will then
+     * be used.
+     */
     FORCE_LATEST
 }
 
