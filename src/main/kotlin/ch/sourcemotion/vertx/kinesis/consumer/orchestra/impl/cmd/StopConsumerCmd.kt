4ef5340c03ed6b569f44f672c0ffbb374551e87b
Michel Werren <michel.werren@husqvarnagroup.com>
2020-11-22 16:01:33 +0100

    feat: Start / stop consumer command dtos with codecs. Remove Jackson codec as no dto will be (de)serialized anymore.

---
+++ src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/impl/cmd/StopConsumerCmd.kt
@@ -0,0 +1,5 @@
+package ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.cmd
+
+import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.ShardId
+
+class StopConsumerCmd(val shardId: ShardId)
