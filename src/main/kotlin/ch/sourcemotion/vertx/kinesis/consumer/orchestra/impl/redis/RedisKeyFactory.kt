149238795c569a0e7d204eae61cc2d8e119ca046
Michel Werren <michel.werren@husqvarnagroup.com>
2020-11-28 13:35:19 +0100

    chore: Orchestra cluster name support on Redis key factory.

--- src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/impl/redis/RedisKeyFactory.kt
+++ src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/impl/redis/RedisKeyFactory.kt
@@ -1,25 +1,27 @@
 package ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.redis
 
+import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.OrchestraClusterName
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.ShardId
 
-class RedisKeyFactory(applicationName: String, streamName: String) {
-    private val orchestraRedisKeyBase = "$applicationName-$streamName"
-    private val shardFinishedKeyBase = "$orchestraRedisKeyBase-finished-"
-    private val shardProgressKeyBase = "$orchestraRedisKeyBase-progress-"
+class RedisKeyFactory(private val clusterName: OrchestraClusterName) {
+    private val shardFinishedKeyBase = "$clusterName-finished-"
+    private val shardProgressKeyBase = "$clusterName-progress-"
+
+    constructor(applicationName: String, streamName: String): this(OrchestraClusterName(applicationName, streamName))
 
     fun createShardProgressFlagKey(shardId: ShardId): String = "${shardProgressKeyBase}$shardId"
     fun createShardProgressFlagKeyWildcard(): String = "${shardProgressKeyBase}*"
 
-    fun createShardSequenceNumberKey(shardId: ShardId): String = "$orchestraRedisKeyBase-sequence-$shardId"
+    fun createShardSequenceNumberKey(shardId: ShardId): String = "$clusterName-sequence-$shardId"
 
     fun createShardFinishedKey(shardId: ShardId): String = "$shardFinishedKeyBase$shardId"
     fun createShardFinishedRedisKeyWildcard(): String = "$shardFinishedKeyBase*"
 
-    fun createDeploymentLockKey() = "$orchestraRedisKeyBase-deployment-lock"
+    fun createDeploymentLockKey() = "$clusterName-deployment-lock"
 
     fun createMergeParentReadyToReshardKey(parentShardId: ShardId, childShardId: ShardId) =
-        "$orchestraRedisKeyBase-ready-for-merge-$childShardId-$parentShardId"
+        "$clusterName-ready-for-merge-$childShardId-$parentShardId"
 
     fun createMergeParentReadyToReshardKeyWildcard(childShardId: ShardId) =
-        "$orchestraRedisKeyBase-ready-for-merge-$childShardId-*"
+        "$clusterName-ready-for-merge-$childShardId-*"
 }
