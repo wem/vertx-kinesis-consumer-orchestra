5c78707d8960a5aa503ccdf697d6eb404b2c4bc7
Michel Werren <michel.werren@source-motion.ch>
2021-06-03 19:30:48 +0200

    #29 chore: Add resharding component tests with a higher number of shards.

--- src/test/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/AbstractComponentTest.kt
+++ src/test/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/AbstractComponentTest.kt
@@ -2,10 +2,12 @@ package ch.sourcemotion.vertx.kinesis.consumer.orchestra
 
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.consumer.AbstractKinesisConsumerCoroutineVerticle
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.codec.LocalCodec
+import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.streamDescriptionWhenActiveAwait
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.testing.*
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.kotest.matchers.shouldBe
 import io.vertx.core.json.JsonObject
+import io.vertx.junit5.Timeout
 import io.vertx.junit5.VertxTestContext
 import io.vertx.kotlin.core.eventbus.completionHandlerAwait
 import kotlinx.coroutines.delay
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import software.amazon.awssdk.services.kinesis.model.Record
 import software.amazon.awssdk.services.kinesis.model.StreamDescription
+import java.util.concurrent.TimeUnit
 
 internal abstract class AbstractComponentTest : AbstractKinesisAndRedisTest(false) {
 
@@ -31,20 +34,6 @@ internal abstract class AbstractComponentTest : AbstractKinesisAndRedisTest(fals
     @BeforeEach
     internal fun setUpComponent() = asyncBeforeOrAfter {
         vertx.eventBus().registerDefaultCodec(FanoutMessage::class.java, LocalCodec("fanout-message-codec"))
-        val streamDescription = kinesisClient.createAndGetStreamDescriptionWhenActive(1)
-        val orchestraOptions = VertxKinesisOrchestraOptions(
-            TEST_APPLICATION_NAME,
-            TEST_STREAM_NAME,
-            region = AWS_REGION,
-            credentialsProviderSupplier = { AWS_CREDENTIALS_PROVIDER },
-            consumerVerticleClass = ComponentTestConsumerVerticle::class.java.name,
-            redisOptions = redisHeimdallOptions,
-            consumerVerticleOptions = JsonObject.mapFrom(ComponentTestConsumerOptions(PARAMETER_VALUE)),
-            loadConfiguration = LoadConfiguration.createConsumeExact(1, NOT_CONSUMED_SHARD_DETECTION_INTERVAL),
-            fetcherOptions = fetcherOptions(streamDescription)
-        )
-
-        orchestra = VertxKinesisOrchestra.create(vertx, orchestraOptions).startAwait()
     }
 
     @AfterEach
@@ -54,6 +43,7 @@ internal abstract class AbstractComponentTest : AbstractKinesisAndRedisTest(fals
 
     @Test
     internal fun consume_some_records(testContext: VertxTestContext) = testContext.async(RECORD_COUNT) { checkpoint ->
+        createStreamAndDeployVKCO()
         eventBus.consumer<FanoutMessage>(RECORD_FAN_OUT_ADDR) { msg ->
             val fanoutMessage = msg.body()
             repeat(fanoutMessage.recordCount) { checkpoint.flag() }
@@ -62,6 +52,68 @@ internal abstract class AbstractComponentTest : AbstractKinesisAndRedisTest(fals
 
         delay(10000)
         kinesisClient.putRecords(1 batchesOf RECORD_COUNT)
+    }
+
+    @Timeout(value = 90, timeUnit = TimeUnit.SECONDS)
+    @Test
+    internal fun split_resharding(testContext: VertxTestContext) = testContext.async(RECORD_COUNT * 2) { checkpoint ->
+        val streamDescription = createStreamAndDeployVKCO(4, 8)
+        eventBus.consumer<FanoutMessage>(RECORD_FAN_OUT_ADDR) { msg ->
+            val fanoutMessage = msg.body()
+            repeat(fanoutMessage.recordCount) { checkpoint.flag() }
+            testContext.verify { fanoutMessage.parameter.shouldBe(PARAMETER_VALUE) }
+        }.completionHandlerAwait()
+
+        delay(10000)
+        kinesisClient.putRecords(RECORD_COUNT batchesOf 1)
+        streamDescription.shards().forEach { shard ->
+            kinesisClient.splitShardFair(shard)
+            kinesisClient.streamDescriptionWhenActiveAwait(TEST_STREAM_NAME)
+        }
+        kinesisClient.putRecords(RECORD_COUNT batchesOf 1)
+    }
+
+    @Timeout(value = 90, timeUnit = TimeUnit.SECONDS)
+    @Test
+    internal fun merge_resharding(testContext: VertxTestContext) = testContext.async(RECORD_COUNT * 2) { checkpoint ->
+        val streamDescription = createStreamAndDeployVKCO(4, 4)
+        eventBus.consumer<FanoutMessage>(RECORD_FAN_OUT_ADDR) { msg ->
+            val fanoutMessage = msg.body()
+            repeat(fanoutMessage.recordCount) { checkpoint.flag() }
+            testContext.verify { fanoutMessage.parameter.shouldBe(PARAMETER_VALUE) }
+        }.completionHandlerAwait()
+
+        delay(10000)
+        kinesisClient.putRecords(RECORD_COUNT batchesOf 1)
+        val shards = streamDescription.shards().iterator()
+        while (shards.hasNext()) {
+            val parent = shards.next()
+            val adjacentParent = shards.next()
+            kinesisClient.mergeShards(parent, adjacentParent)
+            kinesisClient.streamDescriptionWhenActiveAwait(TEST_STREAM_NAME)
+        }
+        kinesisClient.putRecords(RECORD_COUNT batchesOf 1)
+    }
+
+    private suspend fun createStreamAndDeployVKCO(shardCount: Int = 1, consumerCount: Int = 1): StreamDescription {
+        val streamDescription = kinesisClient.createAndGetStreamDescriptionWhenActive(shardCount)
+        val orchestraOptions = VertxKinesisOrchestraOptions(
+            TEST_APPLICATION_NAME,
+            TEST_STREAM_NAME,
+            region = AWS_REGION,
+            credentialsProviderSupplier = { AWS_CREDENTIALS_PROVIDER },
+            consumerVerticleClass = ComponentTestConsumerVerticle::class.java.name,
+            redisOptions = redisHeimdallOptions,
+            consumerVerticleOptions = JsonObject.mapFrom(ComponentTestConsumerOptions(PARAMETER_VALUE)),
+            loadConfiguration = LoadConfiguration.createConsumeExact(
+                consumerCount,
+                NOT_CONSUMED_SHARD_DETECTION_INTERVAL
+            ),
+            fetcherOptions = fetcherOptions(streamDescription)
+        )
+
+        orchestra = VertxKinesisOrchestra.create(vertx, orchestraOptions).startAwait()
+        return streamDescription
     }
 
     protected abstract fun fetcherOptions(streamDescription: StreamDescription): FetcherOptions
