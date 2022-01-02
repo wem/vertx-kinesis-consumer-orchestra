df47decde8ee9f390b100102d237cbfaaf95aae4
Michel Werren <michel.werren@source-motion.ch>
2022-01-02 09:27:29 +0100

    feat: Introduce OrchestraClusterNodeId and make shard id to data object.

--- src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/impl/Types.kt
+++ src/main/kotlin/ch/sourcemotion/vertx/kinesis/consumer/orchestra/impl/Types.kt
@@ -1,12 +1,38 @@
 package ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl
 
+import ch.sourcemotion.vertx.kinesis.consumer.orchestra.VertxKinesisConsumerOrchestraException
 import ch.sourcemotion.vertx.kinesis.consumer.orchestra.impl.ext.isNotNullOrBlank
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+import io.vertx.codegen.annotations.DataObject
+import io.vertx.core.json.JsonObject
 import software.amazon.awssdk.services.kinesis.model.Shard
 
 internal typealias ShardList = List<Shard>
 internal typealias ShardIdList = List<ShardId>
 
-internal data class OrchestraClusterName(val applicationName: String, val streamName: String) {
+data class OrchestraClusterNodeId(val clusterName: String, val nodeId: String) {
+
+    constructor(name: OrchestraClusterName, id: String) : this("$name", id)
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun of(value: String) : OrchestraClusterNodeId {
+            val parts = value.split("::")
+            if (parts.size != 2) {
+                throw VertxKinesisConsumerOrchestraException("$value is not a valid Orchestra cluster node id")
+            }
+            return OrchestraClusterNodeId(parts[0], parts[1])
+        }
+    }
+
+    @JsonValue
+    override fun toString() = "$clusterName::$nodeId"
+}
+
+data class OrchestraClusterName(val applicationName: String, val streamName: String) {
+    @JsonValue
     override fun toString() = "$applicationName-$streamName"
 }
 
@@ -38,7 +64,20 @@ enum class SequenceNumberIteratorPosition { AFTER, AT }
 internal fun String.asSequenceNumberAt() = SequenceNumber(this, SequenceNumberIteratorPosition.AT)
 internal fun String.asSequenceNumberAfter() = SequenceNumber(this, SequenceNumberIteratorPosition.AFTER)
 
+@DataObject
 data class ShardId(val id: String) {
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun create(value: String) = ShardId(value)
+    }
+
+    constructor(json: JsonObject) : this(json.getString("id"))
+
+    fun toJson(): JsonObject = JsonObject().put("id", id)
+
+    @JsonValue
     override fun toString() = id
 }
 
